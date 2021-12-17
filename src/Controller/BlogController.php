1f934468ace49b383af6b4575c78694e8b2b5e9e
HiListen <90248187+HiListen@users.noreply.github.com>
2021-12-17 09:59:18 +0100

    exercice sdofze,klfnklzezegjklvjk

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ArticleRepository;
+use App\Entity\Article;
+
 
 class BlogController extends AbstractController
 {
