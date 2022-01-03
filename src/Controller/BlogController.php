f27567f8e189f036c8c30fdba28b7ee83f66f1e9
HiListen <90248187+HiListen@users.noreply.github.com>
2022-01-03 11:04:44 +0100

    l

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,50 +2,19 @@
 
 namespace App\Controller;
 
-use App\Entity\Contact;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use App\Repository\ArticleRepository;
-use App\Entity\Article;
-
+use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
-    /** 
-    * @var ArticleRepository
+    /**
+     * @Route("/ ", name="blog")
      */
-    private $articleRepository;
-
-    public function __construct(articleRepository $articleRepository)
+    public function index(): Response
     {
-        $this->articleRepository = $articleRepository;
-    }
-    public function contact(Request $request, string $article = ""): Response
-    {
-        $name = $request->query->get('name');
-        $articleForm = new Contact();
-        $form = $this->createForm(ContactType::class, $articleForm);
-        $form->handleRequest($request);
-
-        dump($form->getViewData());
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($form->getData());
-            $entityManager->flush();
-
-            dump('ajout du nouvelle article');
-        }
-
-        return $this->renderForm
-        ('blog/index.html.twig',
-        [
-           'controller_name' => 'Controleur de Article',
-           'id' => $id,
-           'name' => $name,
-           'form' => $form,
-        ]
-        );
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
     }
 }
-
